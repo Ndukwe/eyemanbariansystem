6cd32437a64c4fc9fca2d96bae7c75ff213165fc
Ndukwe Agu <aguemmanuel47@gmail.com>
2017-07-04 13:27:33 +0100

    login and index page

---
+++ clinic/pages/registeration.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Clinic Management System</title>
+<link rel="stylesheet" type="text/css" href="../css/main.css" />
+<link rel="stylesheet" type="text/css" href="../css/menu.css" />
+</head>
+<body>
+  <div id="wrapper">
+<marquee>Welcome to CyPress Clinic...Your Vision is Our Focus</marquee>
+<div id="NavBar">
+	<nav>
+	<ul>
+<li><a href="#">HOME</a></li>
+<li><a href="#">REGISTRATION</a></li>
+<li><a href="#">APPOINTMENT</a></li>
+<li><a href="#">VIEW PATIENT </a></li>
+<li><a href="#">INVENTORY</a></li>
+	</ul>
+   </nav>
+</div>
+<div id="header">
+
+</div>
+<div id="Content">
+
+    <div id="RightContent"></div>
+    <div id="LeftContent"></div>
+
+</div>
+<div id="footer">
+<h3> Copyright Reserved &nbsp<a href="#">mxConnect Design</a></h3>
+</div>
+
+  </div>
+</body>
+<html>
